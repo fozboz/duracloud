920847c7128f699c763309e5ae8b85e821a1877d
Shibo Liu <shibie.liu@utoronto.ca>
2019-06-24 17:45:26 -0400

    WIP

--- common-sns/src/main/java/org/duracloud/common/sns/RabbitMQSubscriptionManager.java
+++ common-sns/src/main/java/org/duracloud/common/sns/RabbitMQSubscriptionManager.java
@@ -1,11 +1,16 @@
 package org.duracloud.common.sns;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.ShutdownSignalException;
+import com.rabbitmq.client.AMQP;
 import org.duracloud.common.error.DuraCloudRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +25,7 @@ public class RabbitMQSubscriptionManager implements SubscriptionManager {
     private String mqUsername;
     private String mqPassword;
     private String exchangeName;
+    private String consumerName;
     private boolean initialized = false;
     private List<MessageListener> messageListeners = new ArrayList<>();
 
@@ -30,6 +36,7 @@ public class RabbitMQSubscriptionManager implements SubscriptionManager {
         mqUsername = username;
         mqPassword = password;
         this.queueName = queueName;
+        consumerName = "consumer-" + queueName;
     }
 
     @Override
@@ -38,7 +45,7 @@ public class RabbitMQSubscriptionManager implements SubscriptionManager {
     }
 
     @Override
-    public void connect() {
+    public synchronized void connect() {
         if (initialized) {
             throw new DuraCloudRuntimeException("this manager is already connected");
         }
@@ -64,9 +71,69 @@ public class RabbitMQSubscriptionManager implements SubscriptionManager {
                 mqChannel.queueBind(queueName, exchangeName, queueName);
             }
 
+            log.info("subscribing consumer {} to queue {} at URL {}", consumerName, queueName, queueUrl);
+            startConsumer();
+
         } catch (Exception ex) {
+            initialized = false;
             log.error("failed to estabilish connection to RabbitMQ with queue name {} and URL {} because {}", queueName, queueUrl, ex.getMessage());
             throw new DuraCloudRuntimeException(ex);
+        }
+
+    }
+
+    private void startConsumer() {
+        try {
+            mqChannel.basicConsume(queueName, false, consumerName,
+                                   new DefaultConsumer(mqChannel) {
+                                       @Override
+                                       public void handleDelivery(String consumerTag,
+                                                                  Envelope envelope,
+                                                                  AMQP.BasicProperties properties,
+                                                                  byte[] body)
+                                           throws IOException {
+                                           String routingKey = envelope.getRoutingKey();
+                                           String contentType = properties.getContentType();
+                                           long deliveryTag = envelope.getDeliveryTag();
+                                           String message = new String(body);
+                                           dispatch(message);
+                                           log.debug("{} dispatched", message);
+                                           mqChannel.basicAck(deliveryTag, false);
+                                           log.debug("{} deleted", message);
+                                       }
+
+
+                                       @Override
+                                       public void handleConsumeOk(String consumerTag) {
+                                           log.info("Consumer registered: {}", consumerTag);
+                                           initialized = true;
+                                       }
+
+                                       @Override
+                                       public void handleCancel(String consumerTag) throws IOException {
+                                           // consumer has been cancelled unexpectedly
+                                           log.debug("consumer has been cancelled unexpectedly: " + consumerTag);
+                                       }
+
+                                       @Override
+                                       public void handleCancelOk(String consumerTag) {
+                                           // consumer has been cancelled explicitly
+                                           log.info("consumer has been cancelled successfully: " + consumerTag);
+                                       }
+
+                                       @Override
+                                       public void handleShutdownSignal(String consumerTag,  ShutdownSignalException sig) {
+                                           // either the channel or the underlying connection has been shut down.
+                                           log.debug("either the channel or the underlying connection has been shut down for consumer {} because {}", consumerTag, sig.getReason().toString());
+                                           initialized = false;
+                                       }
+
+                                   });
+
+
+        } catch (Exception e){
+            log.debug("consumer failed to subscribe: " + e.getMessage(), e);
+            initialized = false;
         }
 
     }
@@ -87,8 +154,36 @@ public class RabbitMQSubscriptionManager implements SubscriptionManager {
         }
     }
 
+    private void cancelConsumer(){
+        try {
+            mqChannel.basicCancel(consumerName);
+            log.info("unsubscripbed consumer {}", consumerName);
+        }catch(IOException e){
+            log.info("error unsubscribing consumer {}", consumerName, e);
+        }
+    }
+
+    private void deleteQueue(){
+        try {
+            mqChannel.queueDelete(queueName);
+            log.info("deleted queue {}", queueName);
+        }catch(IOException e){
+            log.info("error deleting queue {}", queueName, e);
+        }
+    }
+
+
     @Override
     public void disconnect() {
-
+        if (!this.initialized) {
+            throw new DuraCloudRuntimeException("this manager is already disconnected");
+        }
+        log.info("disconnecting");
+        log.info("unsubscribing {}", consumerName);
+        cancelConsumer();
+        log.info("deleting queue {}", queueName);
+        deleteQueue();
+        this.initialized = false;
+        log.info("disconnection complete");
     }
 }
